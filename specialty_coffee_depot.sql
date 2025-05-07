d767a4fc2ca6ec4270c87248e421b64bf73e425a
John Daryll Sampilingan <johndaryllramos8@gmail.com>
2025-05-07 14:12:23 +0800

    change sql

--- specialty_coffee_depot.sql
+++ specialty_coffee_depot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2025 at 08:03 AM
+-- Generation Time: May 07, 2025 at 08:11 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -74,8 +74,31 @@ CREATE TABLE `cupping_forms` (
 --
 
 INSERT INTO `cupping_forms` (`id`, `user_id`, `user_name`, `submission_date`, `form_date`, `table_no`, `batch_number`, `fragrance_aroma`, `dry`, `break_value`, `quality1`, `quality2`, `fragrance_notes`, `flavor`, `flavor_notes`, `aftertaste`, `aftertaste_notes`, `acidity`, `acidity_intensity`, `acidity_notes`, `body`, `body_level`, `body_notes`, `uniformity`, `uniformity_notes`, `clean_cup`, `clean_cup_notes`, `overall`, `overall_notes`, `balance`, `balance_notes`, `sweetness`, `sweetness_notes`, `defective_cups`, `defect_intensity`, `defect_points`, `total_score`, `final_score`, `comments`) VALUES
-(1, 4, '', '2025-04-07 07:15:25', '2025-04-07', '23', 1, 6.00, 3, 3, '', '', '', 6.00, '', 6.00, '', 8.50, 3, '', 6.00, 3, '', 10, '', 10, '', 10.00, '', 10.00, '', 9, '', 1, 2, 2, 81.50, 79.50, 'wewewqe'),
-(2, 3, '', '2025-04-07 07:15:52', '2025-04-07', '22', 1, 6.00, 3, 3, '', '', '', 8.75, '', 8.75, '', 6.00, 3, '', 6.00, 3, '', 10, '', 10, '', 6.00, '', 6.00, '', 10, '321312', 0, 0, 0, 77.50, 77.50, '32312321312312');
+(7, 3, 'johndaryllramos8@gmail.com', '2025-04-14 17:16:35', '2025-04-14', '34A', 1, 9.50, 5, 1, 'sweet', 'badass', 'eqwe', 9.75, 'dsadsa', 10.00, 'dsadas', 9.75, 4, 'dsadasd', 9.75, 3, 'dsdas', 9, 'dsdasd', 10, 'dsadsa', 9.50, 'dsadasdasdadas', 9.75, 'dsada', 10, 'wewqeqw', 4, 3, 12, 97.00, 85.00, 'testing testing'),
+(8, 5, 'felsonecaragao@gmail.com', '2025-04-14 18:06:07', '2025-04-14', '22', 6, 9.75, 5, 5, 'q1', 'q2', 'fa', 9.50, 'fs', 9.75, 'at', 9.75, 1, 'ac', 10.00, 5, 'bd', 9, 'umt', 10, '', 9.75, 'on', 9.75, 'balance', 10, '', 2, 2, 4, 97.25, 93.25, 'notes/comments'),
+(9, 6, 'johndaryllramos23@gmail.com', '2025-04-16 21:26:51', '2025-04-16', '32C', 5, 9.75, 5, 5, 'X1', 'X12', 'SD', 9.50, 'SDAS', 10.00, 'DSAD', 6.00, 2, 'DSA', 9.75, 4, 'DSADAS', 9, 'DSADAS', 9, 'dsdsadas', 9.50, 'dsadas', 8.50, 'dsdasdasdsadsa', 9, 'dsadsaas', 2, 2, 4, 90.00, 86.00, 'fdsfdsfdsf testing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `remember_tokens`
+--
+
+CREATE TABLE `remember_tokens` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `token_hash` varchar(64) NOT NULL,
+  `expires_at` datetime NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `remember_tokens`
+--
+
+INSERT INTO `remember_tokens` (`id`, `user_id`, `token_hash`, `expires_at`, `created_at`) VALUES
+(1, 1, 'c85aa24e74761a91281a870479fa26926f6d37e866a4ace58bbcb91a43b90c63', '2025-05-14 12:53:45', '2025-04-14 10:53:45'),
+(2, 5, 'a7729e9c5370aa069928f7078deaedeb4adb13efe684a5c51cbceeb3613eda50', '2025-05-14 12:56:48', '2025-04-14 10:56:48');
 
 -- --------------------------------------------------------
 
@@ -97,8 +120,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `role`) VALUES
 (1, '', 'admin@specialtycoffeedepotph.com', 'c0b9f07001f97804f5bce76941e9f403c6db8485ee9121c352400ede71843345', 'admin'),
-(3, 'John Daryll Sampilingan', 'johndaryllramos8@gmail.com', '1718c24b10aeb8099e3fc44960ab6949ab76a267352459f203ea1036bec382c2', 'user'),
-(4, 'Johnny gayo', 'johnny.gayo@specialtycoffeedepotph.com', '932f3c1b56257ce8539ac269d7aab42550dacf8818d075f0bdf1990562aae3ef', 'user');
+(3, 'John Daryll Sampilingan', 'johndaryllramos8@gmail.com', '932f3c1b56257ce8539ac269d7aab42550dacf8818d075f0bdf1990562aae3ef', 'user'),
+(4, 'Johnny gayo', 'johnny.gayo@specialtycoffeedepotph.com', '932f3c1b56257ce8539ac269d7aab42550dacf8818d075f0bdf1990562aae3ef', 'user'),
+(5, 'Felsone Caragao', 'felsonecaragao@gmail.com', '5751a44782594819e4cb8aa27c2c9d87a420af82bc6a5a05bc7f19c3bb00452b', 'user'),
+(6, 'Princess Ann Sampilingan', 'johndaryllramos23@gmail.com', '1718c24b10aeb8099e3fc44960ab6949ab76a267352459f203ea1036bec382c2', 'user');
 
 --
 -- Indexes for dumped tables
@@ -108,6 +133,13 @@ INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `role`) VALUES
 -- Indexes for table `cupping_forms`
 --
 ALTER TABLE `cupping_forms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `remember_tokens`
+--
+ALTER TABLE `remember_tokens`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
@@ -126,13 +158,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cupping_forms`
 --
 ALTER TABLE `cupping_forms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `remember_tokens`
+--
+ALTER TABLE `remember_tokens`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -143,6 +181,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `cupping_forms`
   ADD CONSTRAINT `cupping_forms_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `remember_tokens`
+--
+ALTER TABLE `remember_tokens`
+  ADD CONSTRAINT `remember_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
